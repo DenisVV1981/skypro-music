ddcfbddf41699c600db15ac652ab62fb976bca04
Denis Vasilev <d89615022404@gmail.com>
2023-12-01 14:12:49 +0100

    add react comp filter search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import SidebarUserInfo from './components/SideBar/SidebarUserInfo.jsx';
 import SidebarContent from './components/SideBar/SidebarContent.jsx';
 import PlaylistContent from './components/Playlist/PlaylistContent.jsx';
 import MainNavigation from './components/Navigation/MainNavigation.jsx';
+import FilterContent from './components/FilterContent/Filter.jsx';
+import SearchContent from './components/SearchContent/SearchContent.jsx';
+
 
 function App() {
   return (
@@ -14,44 +17,22 @@ function App() {
         <main className="main">
           < MainNavigation />
           <div className="main__centerblock centerblock">
-            <div className="centerblock__search search">
-              <svg className="search__svg">
-                <use xlinkHref ="img/icon/sprite.svg#icon-search"></use>
-              </svg>
-              <input
-                className="search__text"
-                type="search"
-                placeholder="Поиск"
-                name="search"
-              />
-            </div>
+              < SearchContent />
             <h2 className="centerblock__h2">Треки</h2>
-            <div className="centerblock__filter filter">
-              <div className="filter__title">Искать по:</div>
-              <div className="filter__button button-author _btn-text">
-                исполнителю
-              </div>
-              <div className="filter__button button-year _btn-text">
-                году выпуска
-              </div>
-              <div className="filter__button button-genre _btn-text">жанру</div>
-            </div>
-             < PlaylistContent />
+              < FilterContent />
+              < PlaylistContent />
           </div>
           <div className="main__sidebar sidebar">
-
-            < SidebarUserInfo />
-            < SidebarContent />
-            
+              < SidebarUserInfo />
+              < SidebarContent />
           </div>
         </main>
         <div className="bar">
           <div className="bar__content">
             <div className="bar__player-progress"></div>
             <div className="bar__player-block">
-              < Player />
-
-              < Volume />
+                < Player />
+                < Volume />
             </div>
           </div>
         </div>
