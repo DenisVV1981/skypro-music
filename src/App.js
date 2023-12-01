26d6972c6f5f9cb030fef1be473735b87d5941c6
Denis Vasilev <d89615022404@gmail.com>
2023-12-01 12:58:59 +0100

    удалил prettier( постоянные ошибки), создал реакт компонеты sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Player from './components/Player/Player.jsx';
 import Volume from './components/Volume/Volume.jsx';
+import SidebarUserInfo from './components/SideBar/SidebarUserInfo.jsx';
+import SidebarContent from './components/SideBar/SidebarContent.jsx';
 
 function App() {
   return (
@@ -611,45 +613,10 @@ function App() {
             </div>
           </div>
           <div className="main__sidebar sidebar">
-            <div className="sidebar__personal">
-              <p className="sidebar__personal-name">Sergey.Ivanov</p>
-              <div className="sidebar__icon">
-                <svg alt="logout">
-                  <use xlinkHref ="img/icon/sprite.svg#logout"></use>
-                </svg>
-              </div>
-            </div>
-            <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+
+            < SidebarUserInfo />
+            < SidebarContent />
+            
           </div>
         </main>
         <div className="bar">
